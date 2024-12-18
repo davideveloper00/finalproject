efe2eba58cc1076569cb7d68329078d7c76613af
David Lopez Piedrahita <lopezpiedrahita.7@teleperformance.co>
2024-12-17 20:45:56 -0500

    First Push

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, flash, redirect, render_template, request, session
+
+
+
+# Configure application
+app = Flask(__name__)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    if request.method == "GET":
+        return render_template("index.html")
+
+app.run(host='0.0.0.0', port=3000)
+
+
